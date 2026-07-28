85ca5bd498e8f13a122d7e64fbe94e56be66f92f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-07-28 15:45:45 +0200

    feat: update Gradle dependencies and JVM toolchain version

--- surf-discord-webhook-api-api/build.gradle.kts
+++ surf-discord-webhook-api-api/build.gradle.kts
@@ -10,15 +10,17 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     implementation("io.ktor:ktor-client-core:2.3.7")
     implementation("io.ktor:ktor-client-cio:2.3.7")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+    implementation("ch.qos.logback:logback-classic:1.5.38")
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 publishing {
